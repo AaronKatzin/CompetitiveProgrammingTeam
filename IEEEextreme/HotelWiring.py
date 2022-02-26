5050bcbe6cc95f1acb602f9a29acc298cb98e12f
Aaron Katzin <katzin.aaron@gmail.com>
2022-02-26 15:43:04 +0200

    First attempt at HotelWiring
    Score: 11.11

--- IEEEextreme/HotelWiring.py
+++ IEEEextreme/HotelWiring.py
@@ -20,11 +20,29 @@ def get_number():
         return float(data)
 
 # numpy and scipy are available for use
-import numpy
-import scipy
+#import numpy
+#import scipy
 
-a = get_number()
-b = get_number()
+cases = get_number()
 
-res = a + b
-print(res)
+for case in range(cases):
+    correctly_wired_rooms_per_floor = []
+    working_rooms = 0
+    floors = get_number()
+    rooms_per_floor = get_number()
+    switches_off = get_number()
+    #print("switches_off: ", switches_off)
+    for floor in range(floors):
+        correctly_wired_rooms = get_number()
+        correctly_wired_rooms_per_floor.append(correctly_wired_rooms)
+    #print(correctly_wired_rooms_per_floor)
+    correctly_wired_rooms_per_floor.sort()
+    #print("[:switches_off]: ", correctly_wired_rooms_per_floor[:switches_off])
+    for floor in range(switches_off):
+        #print("first loop room: ", room)
+        working_rooms += (rooms_per_floor - correctly_wired_rooms_per_floor[floor]) * 1
+    for floor in range(switches_off, floors - switches_off + 1):
+        #print("second loop room: ", room)
+        working_rooms += correctly_wired_rooms_per_floor[floor]
+    #print("-----> working_rooms: ", working_rooms)
+    print(working_rooms)
