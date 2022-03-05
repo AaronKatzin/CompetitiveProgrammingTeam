9f826af3cd048d6fa42977771c11262ccf083959
Aaron Katzin <katzin.aaron@gmail.com>
2022-03-05 10:33:04 +0200

    Simple solution to Bit Soccer using mask and conditional bitwise or of all performance indices
    Score: 100/100 (473 ms - 15.2 MB)

--- IEEEextreme/BitSoccer.py
+++ IEEEextreme/BitSoccer.py
@@ -20,11 +20,30 @@ def get_number():
         return float(data)
 
 # numpy and scipy are available for use
-import numpy
-import scipy
+#import numpy
+#import scipy
 
-a = get_number()
-b = get_number()
+num_of_players = get_number()
+player_performances = []
 
-res = a + b
-print(res)
+# loop over list of player performance indices, catching input
+for performance in range(num_of_players):
+    player_performances.append(get_number())
+
+num_of_queries = get_number()
+
+# loop over queries
+for query in range(num_of_queries):
+    team_performance = get_number()
+
+    # build negative mask
+    mask = ~team_performance
+    sum = 0
+    for performance in player_performances:
+        # if none of the masked bits are lit, the performace index can be bitwise 'or'ed without lighting wrong bits 
+        if (performance & mask) == 0:
+            sum = (sum | performance)
+    if sum == team_performance:
+        print("YES")
+    else:
+        print("NO")
